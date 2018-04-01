140bf5719a4a296ce069f08d8400862b46f19ae0
Goor Lavi <goorlavi1@gmail.com>
2018-04-02 00:30:55 +0300

    change all basic configuration into more advance and soporting + finding usb devices ports

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
-import '../assets/css/bootstrap.min.css';
-import '../assets/css/bootstrap-theme.min.css';
-
-import CloningMode from '../containers/modes/cloningMode';
-
 import '../assets/css/App.css';
+import React, { Component } from 'react';
 
-class App extends Component {
+class App extends React.Component {
   render() {
-
-
     return (
-      <CloningMode />
+      <div>
+        <h1>Hello, Electron!</h1>
+        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+      </div>
     );
   }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
-import pcTransferApp from './reducers';
-import thunk from 'redux-thunk';
-
-let store = createStore(pcTransferApp, compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-), applyMiddleware(thunk));
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div');
@@ -16,11 +8,4 @@ root.id = "root";
 document.body.appendChild( root );
 
 // Now we can render our application into it
-render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
-
-
+render( <App />, document.getElementById('root') );
